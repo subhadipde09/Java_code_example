c3450160dc072cb58fa1965bef843e41e9a743b9
subhadipde09 <subhadipdeece22l65@gmail.com>
2025-03-22 10:34:52 +0530

    code added

--- ExceptionHandle2.java
+++ ExceptionHandle2.java
@@ -2,7 +2,8 @@ import java.io.IOException;
 
 public class ExceptionHandle2 {
     public static void main(String []x) throws Exception{//compile time exception.
+        //throws keyword is use to report java exception
         System.out.println("a");
-        throw  new IOException();
+        throw  new IOException();//throw is use to exception class throwing the object
     }
 }
